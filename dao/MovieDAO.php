5251cc36e8bf31acc71a27d0768e7d62deaa8929
Renan Stefani <renanstefanir@gmail.com>
2023-05-19 01:53:03 -0300

    Página home configurada para resgatar e exibir dados do banco, dashboard estruturada e configurada

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -40,13 +40,75 @@
 
         public function getLatestMovies() {
 
+            $movies = [];
+
+            $stmt = $this->conn->query("SELECT * FROM movies ORDER BY id DESC");
+
+            $stmt->execute();
+
+            if($stmt->rowCount() > 0) {
+
+                $moviesArray = $stmt->fetchAll();
+
+                foreach($moviesArray as $movie) {
+                    $movies[] = $this->buildMovie($movie);
+                }
+
+            }
+
+            return $movies;
+
         }
 
         public function getMoviesByCategory($category) {
 
+            $movies = [];
+
+            $stmt = $this->conn->prepare("SELECT * FROM movies 
+                                        WHERE category = :category 
+                                        ORDER BY id DESC");
+
+            $stmt->bindParam(":category", $category);
+
+            $stmt->execute();
+
+            if($stmt->rowCount() > 0) {
+
+                $moviesArray = $stmt->fetchAll();
+
+                foreach($moviesArray as $movie) {
+                    $movies[] = $this->buildMovie($movie);
+                }
+
+            }
+
+            return $movies;
+
         }
 
         public function getMoviesByUserId($id) {
+
+            $movies = [];
+
+            $stmt = $this->conn->prepare("SELECT * FROM movies 
+                                        WHERE users_id = :users_id ");
+
+            $stmt->bindParam(":users_id", $id);
+
+            $stmt->execute();
+
+            if($stmt->rowCount() > 0) {
+
+                $moviesArray = $stmt->fetchAll();
+
+                foreach($moviesArray as $movie) {
+                    $movies[] = $this->buildMovie($movie);
+                }
+
+            }
+
+            return $movies;
+            
 
         }
 
--- index.php
+++ index.php
@@ -1,48 +1,105 @@
 <?php
     require_once("templates/header.php");
+
+    require_once("dao/MovieDAO.php");
+
+    // DAO dos filmes
+    $movieDao = new MovieDAO($conn, $BASE_URL);
+
+    $latestMovies = $movieDao->getLatestMovies();
+
+    $actionMovies = $movieDao->getMoviesByCategory("Ação");
+
+    $dramaMovies = $movieDao->getMoviesByCategory("Drama");
+
+    $comedyMovies = $movieDao->getMoviesByCategory("Comédia");
+
+    $fantasyMovies = $movieDao->getMoviesByCategory("Fantasia");
+
+    $fictionMovies = $movieDao->getMoviesByCategory("Ficção");
 ?>
     <div id="main-container" class="container-fluid">
         
-        <h2 class="section-title">Filmes novos</h2>
+        <h2 class="section-title">Recém adicionados</h2>
         <p class="section-description">Veja as críticas dos últimos filmes adicionados no MovieStar.</p>
-        <div class="movies-container"></div>
-        <div class="card movie-card">
-            <div class="card-img-top" style="background-image: url('<?= $BASE_URL ?>img/movies/movie_cover.jpg');"></div>
+            <div class="movies-container">
 
-            <div class="card-body">
-                <p class="card-rating">
-                    <i class="fas fa-star"></i>
-                    <span class="rating">9</span>
-                </p>
+                <?php foreach($latestMovies as $movie): ?>
+                    <?php require("templates/movie_card.php"); ?>
+                <?php endforeach; ?>
 
-                <h5 class="card-title">
-                    <a href="#">Título do Filme</a>
-                </h5>
-                
-                <a href="#" class="btn btn-primary rate-btn">Avaliar</a>
-                <a href="#" class="btn btn-primary card-btn">Conhecer</a>
-            </div>
+                <?php if(count($latestMovies) === 0): ?>
+                    <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+                <?php endif; ?>
         </div>
         
         <h2 class="section-title">Ação</h2>
         <p class="section-description">Veja os melhores filmes de ação.</p>
-        <div class="movies-container"></div>
+            <?php foreach($actionMovies as $movie): ?>
+                    <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+
+        <div class="movies-container">
+            <?php if(count($actionMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+            <?php endif; ?>
+        </div>
+
         
         <h2 class="section-title">Drama</h2>
         <p class="section-description">Veja os melhores filmes de drama.</p>
-        <div class="movies-container"></div>
+
+        <div class="movies-container">
+            <?php foreach($dramaMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+
+            <?php if(count($dramaMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+            <?php endif; ?>
+        </div>
+
         
         <h2 class="section-title">Comédia</h2>
         <p class="section-description">Veja os melhores filmes de comédia.</p>
-        <div class="movies-container"></div>
+
+        <div class="movies-container">
+            <?php foreach($comedyMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+
+            <?php if(count($comedyMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+            <?php endif; ?>
+        </div>
+
         
         <h2 class="section-title">Fantasia</h2>
         <p class="section-description">Veja os melhores filmes de fantasia.</p>
-        <div class="movies-container"></div>
+
+        <div class="movies-container">
+            <?php foreach($fantasyMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+
+            <?php if(count($fantasyMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+            <?php endif; ?>
+        </div>
+
         
         <h2 class="section-title">Ficção</h2>
         <p class="section-description">Veja os melhores filmes de ficção.</p>
-        <div class="movies-container"></div>
+        
+        <div class="movies-container">
+            <?php foreach($fictionMovies as $movie): ?>
+                <?php require("templates/movie_card.php"); ?>
+            <?php endforeach; ?>
+
+            <?php if(count($fictionMovies) === 0): ?>
+                <p class="empty-list">Ainda não há filmes cadastrados nessa categoria.</p>
+            <?php endif; ?>
+        </div>
 
     </div>
 
