758971bb6dce37b666863434335e5bd61ad3b21b
Renan Stefani <renanstefanir@gmail.com>
2023-05-24 05:18:56 -0300

    Barra de pesquisa configurada, responsividade implementada, ajustes ao readme do projeto

--- templates/header.php
+++ templates/header.php
@@ -55,7 +55,7 @@
                 <i class="fas fa-bars"></i>
             </button>
 
-            <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+            <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
                 <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar filmes" aria-label="Search">
                 
                 <button class="btn my-2 my-sm-0" type="submit">
